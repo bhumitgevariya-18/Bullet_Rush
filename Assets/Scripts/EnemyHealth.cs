7281031e66c94085734e75e5a338b20f8fcab7bc
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-08-18 20:45:48 +0530

    Enemy destroy
    Added enemy destroy vfx using editor and script

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [SerializeField] GameObject robotExplosionVFX;
     [SerializeField] int startingHealth = 3;
 
     int currentHealth;
@@ -17,6 +18,7 @@ public class EnemyHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            Instantiate(robotExplosionVFX, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
