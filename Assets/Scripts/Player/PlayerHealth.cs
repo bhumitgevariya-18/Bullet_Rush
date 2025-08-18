dee9705421f614828eabc3762ec0c62003e23da2
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-08-18 21:36:36 +0530

    Camera switching
    switched to another virtual camera when player dies,  added in top corner of dome
    also used cinemachine brain blend update method to make a cool transition of cameras

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int startingHealth = 5;
+    [SerializeField] CinemachineVirtualCamera deathvirtualCamera;
+    [SerializeField] Transform weaponCamera;
 
     int currentHealth;
+    int gameOverVirtualCameraPriority = 20;
 
     void Awake()
     {
@@ -18,6 +22,8 @@ public class PlayerHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            weaponCamera.parent = null; // Unparent the weapon camera
+            deathvirtualCamera.Priority = gameOverVirtualCameraPriority; // Set the death camera priority higher so that it can switch to it
             Destroy(this.gameObject);
         }
     }
