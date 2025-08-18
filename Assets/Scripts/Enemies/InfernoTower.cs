7c991ab8a8593c4aae560fa9fbafba6ea705da9f
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-08-19 05:28:02 +0530

    Inferno Ball
    created Inferno tower ball prefab which can be shot by the tower, changed tower script and added spawning location and ball prefab to it
    Also added glowing muzzle in the tower

--- Assets/Scripts/Enemies/InfernoTower.cs
+++ Assets/Scripts/Enemies/InfernoTower.cs
@@ -1,12 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class InfernoTower : MonoBehaviour
 {
+    [SerializeField] GameObject infernoBallPrefab;
     [SerializeField] Transform towerHead; // The part of the tower that rotates and shoots
     [SerializeField] Transform TargetPlayer;
+    [SerializeField] Transform infernoBallSpawnPoint;
+    [SerializeField] float fireRate = 2f;
+
+    PlayerHealth playerHealth;
+
+    private void Start()
+    {
+        playerHealth = FindFirstObjectByType<PlayerHealth>();
+        StartCoroutine(FireInfernoBall());
+    }
 
     private void Update()
     {
         towerHead.LookAt(TargetPlayer); // Make the tower head look at the camera
+    }
+
+    IEnumerator FireInfernoBall()
+    {
+        while(playerHealth)
+        {
+            yield return new WaitForSeconds(fireRate);
+            Instantiate(infernoBallPrefab, infernoBallSpawnPoint.position, towerHead.rotation);
+        }
     }
 }
