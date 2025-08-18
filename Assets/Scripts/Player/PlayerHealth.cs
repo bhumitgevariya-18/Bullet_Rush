e54b4a466c3cbbf0b9209c837a54c2add47f9a6e
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-08-19 02:44:43 +0530

    Shield bars
    Adjusted number of shield health bars at run time using a method in player health script

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int startingHealth = 5;
+    [Range(1, 10)]
+    [SerializeField] int startingHealth = 10;
     [SerializeField] CinemachineVirtualCamera deathvirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars; // UI shield bars to represent health of shield
 
     int currentHealth;
     int gameOverVirtualCameraPriority = 20;
@@ -18,13 +21,28 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        Debug.Log(amount + " damage taken");
+        ManageShieldUI();
 
         if (currentHealth <= 0)
         {
             weaponCamera.parent = null; // Unparent the weapon camera
             deathvirtualCamera.Priority = gameOverVirtualCameraPriority; // Set the death camera priority higher so that it can switch to it
             Destroy(this.gameObject);
+        }
+    }
+
+    void ManageShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].enabled = true; // Enable the shield bar
+            }
+            else
+            {
+                shieldBars[i].enabled = false; // Disable the shield bar
+            }
         }
     }
 }
